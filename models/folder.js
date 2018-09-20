7bcba3ca87f0507da7a2625b3bb99d01bdade5f6
clkent <chelsea.kent@gmail.com>
2018-09-20 12:20:26 -0700

    create folder schema and model

---
+++ models/folder.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const folderSchema = new mongoose.Schema({
+	name: { type: String, required: true, unique: true }
+});
+
+// Add 'createdAt' and 'updatedAt' fields
+folderSchema.set('timestamps', true);
+
+//expose virtuals and remove _id and __v
+folderSchema.set('toObject', {
+	virtuals: true,
+	transform: (doc, res) => {
+		delete res._id;
+		delete res.__v;
+	}
+});
+
+module.exports = mongoose.model('Folder', folderSchema);
