cbe263ff7d77c249a8e0f35ec9d933faee52b526
clkent <chelsea.kent@gmail.com>
2018-09-24 12:07:41 -0700

    update server.js

---
+++ routes/users.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const User = require('../models/users');
+
+const router = express.Router();
+
+/* ========== POST USER ========== */
+
+router.post('/', (req, res, next) => {
+  const { fullName, userName, password } = req.body;
+
+  User.create({ fullName, userName, password })
+    .then(result => {
+      if (result) {
+        const { id } = result;
+        res
+          .location(`/api/users/${id}`)
+          .status(201)
+          .json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+module.exports = router;
